ec7639e03bfe7b2d481111e909559d53b4b4c34d
Nikunj Navdiya <106215707+navdiya-nikunj@users.noreply.github.com>
2022-05-31 20:49:19 +0530

    Update and rename Problem 4 to Problem_4.c

--- Contest-1/Problem_4.c
+++ Contest-1/Problem_4.c
@@ -1,4 +1,4 @@
-Problem 5) Sum of digit of odd index
+/*Problem 5) Sum of digit of odd index
 
 Task:-
 suppose you have a number of 5 digit like 75439. - give the index like : index of 7 is 5 index of 5 is 4 index of 4 is 3 index of 3 is 2 index of 9 is 1
@@ -18,7 +18,7 @@ Sample Output :-
 Explanation :-
 sum of 5 + 3 + 1 = 9
 
-Solution In C language:-
+Solution In C language:-*/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -37,12 +37,3 @@ int main()
     return 0;
 }
 
-Solution In Python:-
-
-sum=0
-n=int(input())
-while n>0:
-    k=n%10
-    n=int(n/100)
-    sum=sum+k
-print(sum)
